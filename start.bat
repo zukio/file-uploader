e94420bbc2adc84ddd8ead68f66b5e693283b3ab
zukio <as.web554@gmail.com>
2024-03-08 19:58:17 +0900

    スタートバッチ

--- start.bat
+++ start.bat
@@ -4,18 +4,19 @@ REM # ShiftJIS / CRLF
 setlocal ENABLEDELAYEDEXPANSION
 CD /D "%~dp0"
 
-set path_project=%~dp0dist\win-unpacked\file-uploader.exe
+set path_project=%~dp0dist\win-unpacked\
+set path_app=%path_project%file-uploader.exe
 
 REM # ウインドウタイトルで重複起動を確認（ブラウザのウインドウタイトル）
 tasklist /FI "WINDOWTITLE eq MainWindow" | findstr "[0-9]" >NUL
 if %ERRORLEVEL% == 1 (
- 	start /min "MainWindow" "%path_project%" --plugins=%~dp0dist\win-unpacked\plugins --readDir=%USERPROFILE%/Pictures --saveDir=%~dp0plugins
+ 	start /min "MainWindow" "%path_app%" --plugins=%path_project%plugins --readDir=%USERPROFILE%/Pictures --saveDir=%~dp0plugins
  	SET /p x=%DATE% %TIME:~0,8%  起動中... <NUL
  	CALL :WAIT 3
  ) else (
  	ECHO 既に起動中
 )
-EXIT /b 0
+EXIT 0
 
 :WAIT
 SET /p x=%1秒待機します <NUL
